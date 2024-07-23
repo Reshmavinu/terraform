728e59a0ce82da056e8a0b65894da24de820a07e
Reshmavinu <158140771+Reshmavinu@users.noreply.github.com>
2024-07-23 09:55:12 +0530

    Update main.tf

--- ec2_vpc_subnet/main.tf
+++ ec2_vpc_subnet/main.tf
@@ -8,3 +8,21 @@ resource "aws_subnet" "subnet_id" {
     Name = "Main"
   }
 }
+resource "aws_internet_gateway" "gw" {
+  vpc_id = aws_vpc.VPC_resource.id
+
+  tags = {
+    Name = "main-gw"
+  }
+}
+resource "aws_route_table" "route_table" {
+  vpc_id = aws_vpc.VPC_resource.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.gw.id
+  }
+  tags = {
+    Name = "RTB"
+  }
+}
